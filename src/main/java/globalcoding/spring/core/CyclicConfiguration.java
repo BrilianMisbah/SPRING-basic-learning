78d084086512536785559c6e1b20befec14542fc
ian <b.misbah@global-coding.com>
2022-10-26 08:23:57 +0700

    add cyclic case

---
+++ src/main/java/globalcoding/spring/core/CyclicConfiguration.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package globalcoding.spring.core;
+
+import globalcoding.spring.core.cyclic.CyclicA;
+import globalcoding.spring.core.cyclic.CyclicB;
+import globalcoding.spring.core.cyclic.CyclicC;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author gloding
+ */
+@Configuration
+public class CyclicConfiguration {
+    @Bean
+    public CyclicA cyclicA(CyclicB cyclicB){
+        return new CyclicA(cyclicB);
+    }
+    
+    @Bean
+    public CyclicB cyclicB(CyclicC cyclicC){
+        return new CyclicB(cyclicC);
+    }
+    
+    @Bean
+    public CyclicC cyclicC(CyclicA cyclicA){
+        return new CyclicC(cyclicA);
+    }
+}
---
+++ src/main/java/globalcoding/spring/core/cyclic/CyclicA.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package globalcoding.spring.core.cyclic;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ *
+ * @author gloding
+ */
+@AllArgsConstructor
+@Data
+public class CyclicA {
+    private CyclicB cyclicB;
+}
