0380ab62b42eb7f1c57c66f588bca83dd4f6977d
ian <b.misbah@global-coding.com>
2022-10-26 08:20:59 +0700

    add basic application test

---
+++ src/test/java/globalcoding/spring/core/BasicSpringLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package globalcoding.spring.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
