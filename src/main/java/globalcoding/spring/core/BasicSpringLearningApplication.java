cf3ef7368d4a335661dc208fb7857ef8276ea3cb
ian <b.misbah@global-coding.com>
2022-10-26 08:20:18 +0700

    add app

---
+++ src/main/java/globalcoding/spring/core/BasicSpringLearningApplication.java
@@ -0,0 +1,13 @@
+package globalcoding.spring.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSpringLearningApplication.class, args);
+    }
+
+}
