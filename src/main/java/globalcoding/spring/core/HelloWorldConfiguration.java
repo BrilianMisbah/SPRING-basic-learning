b64d9d6476af558d6f78d048df112d9fed00ee85
ian <b.misbah@global-coding.com>
2022-10-26 08:21:25 +0700

    add hello world configuration

---
+++ src/main/java/globalcoding/spring/core/HelloWorldConfiguration.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package globalcoding.spring.core;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author gloding
+ */
+
+@Configuration
+public class HelloWorldConfiguration {
+    
+}
